76f7158d2d1184b6cd5949b79ae1fce5d2ee1cf5
Steve Lloyd <steve@stevelloyd.net>
2014-07-09 20:44:54 +0100

    added in pull up/down definitions

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
-# WiringPi interface library to Racket
+# Racket interface library to WiringPi
 
 ## Introduction
 
-[WiringPi](http://wiringpi.com/contact/) is a Raspberry Pi GPIO access library written in C
+[WiringPi](http://wiringpi.com/contact/) is a Raspberry Pi GPIO access library written in C. It provides an implementation of the Arduino Wiring functions for the Raspberry Pi.
+
+This library provides simple access to WiringPi functions using Racket's ffi (foreign function interface.
--- wiringpi.rkt
+++ wiringpi.rkt
@@ -17,14 +17,32 @@
 (define-wiringpi wiringPiI2CReadReg16 (_fun _int _int -> _int))
 (define-wiringpi wiringPiSetup (_fun -> _int))
 (define-wiringpi pinMode (_fun _int _int -> _void))
+(define-wiringpi pullUpDnControl (_fun _int _int -> _void))
 (define-wiringpi digitalRead (_fun _int -> _int))
 (define-wiringpi digitalWrite (_fun _int _int -> _void))
+
+(define WPI_MODE_PINS         0)
+(define WPI_MODE_GPIO         1)
+(define WPI_MODE_GPIO_SYS     2)
+(define WPI_MODE_PHYS         3)
+(define WPI_MODE_PIFACE       4)
+(define WPI_MODE_UNINITIALISED -1)
+
+;; pin modes
 
 (define	INPUT       0)
 (define	OUTPUT	    1)
 (define	PWM_OUTPUT  2)
 (define	GPIO_CLOCK  3)
 (define	SOFT_PWM_OUTPUT	 4)
+(define SOFT_TONE_OUTPUT 5)
 
 (define	LOW 0)
 (define	HIGH 1)
+
+;; pull up/down/none
+
+(define PUD_OFF         0)
+(define PUD_DOWN        1)
+(define PUD_UP          2)
+
