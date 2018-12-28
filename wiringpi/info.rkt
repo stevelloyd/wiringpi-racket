a817140ed01d70f80bc6e4e4fef2aeaf303ee242
Steve Lloyd <stevel@kinoteki.com>
2018-12-28 14:28:12 +0000

    import old library

--- wiringpi/info.rkt
+++ wiringpi/info.rkt
@@ -3,6 +3,6 @@
 (define deps '("base"))
 (define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
 (define scribblings '(("scribblings/wiringpi.scrbl" ())))
-(define pkg-desc "Description Here")
-(define version "0.0")
+(define pkg-desc "A Racket interface to the Wiringpi library")
+(define version "0.1")
 (define pkg-authors '(stevel))
